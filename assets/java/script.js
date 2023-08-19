300bee907ca3158845df08e4337064d1b945e1c2
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-08-18 20:27:04 -0400

    added color styling to time blocks based on relative time

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,19 +1,33 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+
+console.log(dayjs());
 
 $(function () {
-  //
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
 
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?\
+  var currentHour = dayjs().hour();
+  var timeBlockElements = document.querySelectorAll(".time-block");
+
+  for (var i = 0; i < timeBlockElements.length; i++) {
+    var elementId = timeBlockElements[i].id;
+    var timeBlockHour = parseInt(elementId.split("-")[1]);
+
+    if (timeBlockHour < currentHour) {
+      timeBlockElements[i].classList.add("past");
+    } else if (timeBlockHour === currentHour) {
+      timeBlockElements[i].classList.add("present");
+    } else {
+      timeBlockElements[i].classList.add("future");
+    }
+  }
 
   // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
   // Declare function to read an 'description' array from local storage or create one if it's not already there.
   function readDescriptionFromStorage() {
     var description = localStorage.getItem("events");
     if (description) {
-      description = JSON.parse(projects);
+      description = JSON.parse(description);
     } else {
       description = [];
     }
@@ -21,11 +35,11 @@ $(function () {
   }
 
   function saveDescriptionToStorage(description) {
-    localStorage.setItem('description', JSON.stringify(description))
+    localStorage.setItem("description", JSON.stringify(description));
   }
 
   //Declare a function to empty the textareas of descriptions, get descriptions from local storage,
-  var descriptionDisplayEl = $('#description')
+  var descriptionDisplayEl = $("#description");
   function displayDescriptionData() {
     descriptionDisplayEl.empty();
     var description = readDescriptionFromStorage();
